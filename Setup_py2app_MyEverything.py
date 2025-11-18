2fc027e8b6e16b71ae8145752d128b1d9b99a4d9
Dennis H Mathes <dennyrgood@yahoo.com>
2025-11-18 12:23:22 +0000

    MyEverything - Program Finished - GUI Pretty

---
+++ Setup_py2app_MyEverything.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+Setup script for creating a macOS application bundle using py2app.
+
+Usage:
+    python setup.py py2app
+
+This will create a standalone .app bundle in the dist/ folder.
+"""
+
+from setuptools import setup
+
+APP = ['MyEverything.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': False,
+    'packages': ['tkinter'],
+    'iconfile': 'app_icon.icns',  # Optional: Add your custom icon file
+    'plist': {
+        'CFBundleName': 'MyEverything',
+        'CFBundleDisplayName': 'MyEverything',
+        'CFBundleGetInfoString': 'macOS Find GUI',
+        'CFBundleIdentifier': 'com.myeverything.app',
+        'CFBundleVersion': '1.0.0',
+        'CFBundleShortVersionString': '1.0.0',
+        'NSHumanReadableCopyright': 'Copyright © 2025',
+    }
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
