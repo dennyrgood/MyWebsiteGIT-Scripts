36b1ea7047ed756ed98a87ac090d40591a7b8ae4
Dennis H Mathes <dennyrgood@yahoo.com>
2025-11-18 19:08:45 +0100

    fix get-clone

--- get-clone-PlaceObjects.sh
+++
@@ -1,9 +0,0 @@
-# cd ~/Documents/MyWebsiteGIT/
-
-# git clone https://github.com/dennyrgood/PlaceObjects
-
-# Example to clone the VisionOS branch
-
-# git clone --branch copilot/build-visionos-app-apple-vision-pro https://github.com/dennyrgood/PlaceObjects
-
-
