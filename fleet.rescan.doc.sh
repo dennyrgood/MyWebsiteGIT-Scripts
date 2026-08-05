97becdc34383c680df1b1ca43804335c00839f77
Dennis H Mathes <dennyrgood@yahoo.com>
2026-08-05 18:57:35 +0200

    clean up scripts

--- fleet.rescan.doc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ~/repos && for r in scripts dennyrgood.github.io standing-up-llm google-photos; do
-
-  (cd "$r" && git ls-files -s | sed "s|^|$r/|")
-
-done > ~/fleet_git_baseline_NEW.txt
-
-diff <(sort ~/fleet_git_baseline.txt) <(sort ~/fleet_git_baseline_NEW.txt)
-
